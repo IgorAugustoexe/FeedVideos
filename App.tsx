bac9d34ee1973287f5e0d709c92c1de3e16ae77a
Igor Augusto <igoraugusto.dev@gmail.com>
2024-10-14 01:23:35 -0300

    feedVideos ready

---
+++ App.tsx
@@ -0,0 +1,84 @@
+import React, {useCallback, useRef, useState} from 'react'
+import {FlatList, StyleSheet, Text, TouchableOpacity, View} from 'react-native'
+import Video, {VideoRef} from 'react-native-video'
+import {config} from './Styles'
+import {ListVideos} from './ListVideos'
+
+function App(): React.JSX.Element {
+  //const videoRef = useRef<VideoRef>(null);
+  const videoRef = useRef<any>({})
+  const predecessorIndexVideo = useRef<number>(-1)
+  const highestIndex = useRef<number>(0)
+
+  const [currentIndexVideo, setCurrentIndexVideo] = useState<number>(0)
+
+  const [listVideos, setListVideos] = useState<any>(ListVideos)
+
+  // Unpause only the video currently being displayed in the Flatlist, and when scrolling, pause the current video, unpause the next video in the list, and pause all others.
+  const onViewableItemsChanged = useCallback((viewableItems: any) => {
+    if (predecessorIndexVideo.current >= 0 && videoRef.current[predecessorIndexVideo.current]) {
+      videoRef.current[predecessorIndexVideo.current].seek(0)
+    }
+    setCurrentIndexVideo(viewableItems.changed[0].index)
+    if (highestIndex.current <= viewableItems.changed[0].index) highestIndex.current = viewableItems.changed[0].index
+    predecessorIndexVideo.current = viewableItems.changed[0].index
+  }, [])
+
+  // Control to manually pause the videos by clicking anywhere on the screen.
+  const pauseVideoControl = (index: number) => {
+    if (currentIndexVideo != -1) {
+      setCurrentIndexVideo(-1)
+      return
+    }
+    setCurrentIndexVideo(index)
+  }
+
+  const VideoComponent = ({item, index}: any) => (
+    <TouchableOpacity onPress={() => pauseVideoControl(index)} activeOpacity={0.5}>
+      <Video
+        style={styles.backgroundVideo}
+        source={{
+          uri: item.url,
+        }}
+        poster={item.thumbnail}
+        //ref={videoRef}
+        ref={ref => (videoRef.current[index] = ref)}
+        onError={err => console.log(index + ' - ' + JSON.stringify(err))}
+        paused={currentIndexVideo == index ? false : true}
+      />
+      <Text style={{position: 'absolute', bottom: 0, marginLeft: 15}}>video {index}</Text>
+    </TouchableOpacity>
+  )
+
+  return (
+    <View style={{flex: 1, backgroundColor: '#000'}}>
+      <FlatList
+        data={listVideos}
+        style={{width: config.windowWidth, height: config.windowHeight, position: 'absolute'}}
+        pagingEnabled
+        showsVerticalScrollIndicator={false}
+        keyExtractor={item => item.id}
+        focusable
+        onViewableItemsChanged={onViewableItemsChanged}
+        viewabilityConfig={{viewAreaCoveragePercentThreshold: 50}}
+        renderItem={VideoComponent}
+      />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  backgroundVideo: {
+    //position: 'absolute',
+    top: 0,
+    left: 0,
+    bottom: 0,
+    right: 0,
+    width: config.windowWidth,
+    height: config.windowHeight,
+    borderWidth: 1,
+    borderColor: '#00f',
+  },
+})
+
+export default App
---
+++ ListVideos.ts
@@ -0,0 +1,122 @@
+export const ListVideos = [
+  {
+    id: 0,
+    thumbnail: 'https://d4610frptnos4.cloudfront.net/assets01/1000000063/videos/7174790731461102854.jpg',
+    url: 'https://d4610frptnos4.cloudfront.net/assets01/1000000063/videos/7174790731461102854.mp4',
+  },
+  {
+    id: 1,
+    thumbnail: 'https://d4610frptnos4.cloudfront.net/assets01/1000000065/videos/7194505462161657094.jpg',
+    url: 'https://d4610frptnos4.cloudfront.net/assets01/1000000065/videos/7194505462161657094.mp4',
+  },
+  {
+    id: 2,
+    thumbnail: 'https://d4610frptnos4.cloudfront.net/assets01/1000000060/videos/7169989304041737478.jpg',
+    url: 'https://d4610frptnos4.cloudfront.net/assets01/1000000060/videos/7169989304041737478.mp4',
+  },
+  {
+    id: 3,
+    thumbnail: 'https://d4610frptnos4.cloudfront.net/assets01/1000000063/videos/7174790731461102854.jpg',
+    url: 'https://d4610frptnos4.cloudfront.net/assets01/1000000063/videos/7174790731461102854.mp4',
+  },
+  {
+    id: 4,
+    thumbnail: 'https://d4610frptnos4.cloudfront.net/assets01/1000000065/videos/7194505462161657094.jpg',
+    url: 'https://d4610frptnos4.cloudfront.net/assets01/1000000065/videos/7194505462161657094.mp4',
+  },
+  {
+    id: 5,
+    thumbnail: 'https://d4610frptnos4.cloudfront.net/assets01/1000000060/videos/7169989304041737478.jpg',
+    url: 'https://d4610frptnos4.cloudfront.net/assets01/1000000060/videos/7169989304041737478.mp4',
+  },
+  {
+    id: 6,
+    thumbnail: 'https://d4610frptnos4.cloudfront.net/assets01/1000000063/videos/7174790731461102854.jpg',
+    url: 'https://d4610frptnos4.cloudfront.net/assets01/1000000063/videos/7174790731461102854.mp4',
+  },
+  {
+    id: 7,
+    thumbnail: 'https://d4610frptnos4.cloudfront.net/assets01/1000000065/videos/7194505462161657094.jpg',
+    url: 'https://d4610frptnos4.cloudfront.net/assets01/1000000065/videos/7194505462161657094.mp4',
+  },
+  {
+    id: 8,
+    thumbnail: 'https://d4610frptnos4.cloudfront.net/assets01/1000000060/videos/7169989304041737478.jpg',
+    url: 'https://d4610frptnos4.cloudfront.net/assets01/1000000060/videos/7169989304041737478.mp4',
+  },
+  {
+    id: 9,
+    thumbnail: 'https://d4610frptnos4.cloudfront.net/assets01/1000000063/videos/7174790731461102854.jpg',
+    url: 'https://d4610frptnos4.cloudfront.net/assets01/1000000063/videos/7174790731461102854.mp4',
+  },
+  {
+    id: 10,
+    thumbnail: 'https://d4610frptnos4.cloudfront.net/assets01/1000000065/videos/7194505462161657094.jpg',
+    url: 'https://d4610frptnos4.cloudfront.net/assets01/1000000065/videos/7194505462161657094.mp4',
+  },
+  {
+    id: 11,
+    thumbnail: 'https://d4610frptnos4.cloudfront.net/assets01/1000000060/videos/7169989304041737478.jpg',
+    url: 'https://d4610frptnos4.cloudfront.net/assets01/1000000060/videos/7169989304041737478.mp4',
+  },
+  {
+    id: 12,
+    thumbnail: 'https://d4610frptnos4.cloudfront.net/assets01/1000000063/videos/7174790731461102854.jpg',
+    url: 'https://d4610frptnos4.cloudfront.net/assets01/1000000063/videos/7174790731461102854.mp4',
+  },
+  {
+    id: 13,
+    thumbnail: 'https://d4610frptnos4.cloudfront.net/assets01/1000000065/videos/7194505462161657094.jpg',
+    url: 'https://d4610frptnos4.cloudfront.net/assets01/1000000065/videos/7194505462161657094.mp4',
+  },
+  {
+    id: 14,
+    thumbnail: 'https://d4610frptnos4.cloudfront.net/assets01/1000000060/videos/7169989304041737478.jpg',
+    url: 'https://d4610frptnos4.cloudfront.net/assets01/1000000060/videos/7169989304041737478.mp4',
+  },
+  {
+    id: 15,
+    thumbnail: 'https://d4610frptnos4.cloudfront.net/assets01/1000000063/videos/7174790731461102854.jpg',
+    url: 'https://d4610frptnos4.cloudfront.net/assets01/1000000063/videos/7174790731461102854.mp4',
+  },
+  {
+    id: 16,
+    thumbnail: 'https://d4610frptnos4.cloudfront.net/assets01/1000000065/videos/7194505462161657094.jpg',
+    url: 'https://d4610frptnos4.cloudfront.net/assets01/1000000065/videos/7194505462161657094.mp4',
+  },
+  {
+    id: 17,
+    thumbnail: 'https://d4610frptnos4.cloudfront.net/assets01/1000000060/videos/7169989304041737478.jpg',
+    url: 'https://d4610frptnos4.cloudfront.net/assets01/1000000060/videos/7169989304041737478.mp4',
+  },
+  {
+    id: 18,
+    thumbnail: 'https://d4610frptnos4.cloudfront.net/assets01/1000000063/videos/7174790731461102854.jpg',
+    url: 'https://d4610frptnos4.cloudfront.net/assets01/1000000063/videos/7174790731461102854.mp4',
+  },
+  {
+    id: 19,
+    thumbnail: 'https://d4610frptnos4.cloudfront.net/assets01/1000000065/videos/7194505462161657094.jpg',
+    url: 'https://d4610frptnos4.cloudfront.net/assets01/1000000065/videos/7194505462161657094.mp4',
+  },
+  {
+    id: 20,
+    thumbnail: 'https://d4610frptnos4.cloudfront.net/assets01/1000000060/videos/7169989304041737478.jpg',
+    url: 'https://d4610frptnos4.cloudfront.net/assets01/1000000060/videos/7169989304041737478.mp4',
+  },
+  {
+    id: 21,
+    thumbnail: 'https://d4610frptnos4.cloudfront.net/assets01/1000000063/videos/7174790731461102854.jpg',
+    url: 'https://d4610frptnos4.cloudfront.net/assets01/1000000063/videos/7174790731461102854.mp4',
+  },
+  {
+    id: 22,
+    thumbnail: 'https://d4610frptnos4.cloudfront.net/assets01/1000000065/videos/7194505462161657094.jpg',
+    url: 'https://d4610frptnos4.cloudfront.net/assets01/1000000065/videos/7194505462161657094.mp4',
+  },
+  {
+    id: 23,
+    thumbnail: 'https://d4610frptnos4.cloudfront.net/assets01/1000000060/videos/7169989304041737478.jpg',
+    url: 'https://d4610frptnos4.cloudfront.net/assets01/1000000060/videos/7169989304041737478.mp4',
+  },
+]
